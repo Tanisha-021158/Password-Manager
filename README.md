1ed0b75e8add9f1572e034554a17a9679adb582d
Tanisha-021158 <tanishasaha225@gmail.com>
2024-12-27 22:01:42 +0530

    Project added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password-Manager
+
+#encode- converts into bytes
+
+#str creates b'hello' which is bytes...because of str()
+#use .decode
--- code.py
+++ code.py
@@ -1,14 +1,30 @@
 from cryptography.fernet import Fernet
 
+#key+password+text to encrypt= random text
+#random text+key+password=text to encrypt
+
+def write_key():
+    key=Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    file=open("key.key","rb")
+    key=file.read()
+    file.close()
+    return key
+
 master_p=input("Enter your masterpassword:")
+key=load_key()+master_p.encode()
+fer=Fernet(key)
 print("View/Add")
 
 def view():
     with open('password.txt','r') as f:
         for line in f.readlines():
-            data=line.rstrip()
-            user,passw=data.split("  ")
-            print("User:",user,"Password:",passw)
+            line=line.rstrip()
+            user,passw=line.split("  ",1)
+            print("User:",user,"Password:",fer.decrypt(passw.encode()).decode())
 
 def existing_user(name):
     with open ('password.txt','r') as f:
@@ -26,7 +42,7 @@ def add():
     if check==False:
         p=input("Enter your password:")
         with open('password.txt','a') as f:
-            f.write(name+"  "+p+"\n")
+            f.write(name+"  "+fer.encrypt(p.encode()).decode()+"\n")
 
 while True:
     ch=input("Enter your choice(Enter q to quit):").lower()
